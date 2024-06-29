01531386e6d7b0f0bfa869291a1cec873a6470e8
Jezreel Kintino de Moraes <69700303+Jezreel-Moraes@users.noreply.github.com>
2024-06-29 19:42:39 -0300

    starting

---
+++ login/script.js
@@ -0,0 +1,18 @@
+function LoginController() {
+  this.reference = document.querySelector(".js-login-container");
+  this.username = this.reference.querySelector(".js-username");
+  this.password = this.reference.querySelector(".js-password");
+  this.submitButton = this.reference.querySelector(".js-submit");
+
+  this.submitButton.addEventListener("click", function(event) {
+    event.preventDefault()
+    if (username.value != "admin") return;
+    if (password.value != "admin") return;
+
+    window.location = "/home";
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let loginController = new LoginController();
+});
