031d04c7371bc1ad40dc9592bc3cf927f684e3ae
Jezreel Kintino de Moraes <69700303+Jezreel-Moraes@users.noreply.github.com>
2024-06-30 13:29:53 -0300

    ajustando css login

--- login/src/javascript/script.js
+++ login/src/javascript/script.js
@@ -1,5 +1,5 @@
 function LoginController() {
-  this.reference = document.querySelector(".js-login-container");
+  this.reference = document.querySelector(".js-login-form-container");
   this.username = this.reference.querySelector(".js-username");
   this.password = this.reference.querySelector(".js-password");
   this.submitButton = this.reference.querySelector(".js-submit");
