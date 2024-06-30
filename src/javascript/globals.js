edf1771d55609b0b5bcf16038be688f30c17ae14
Jezreel Kintino de Moraes <69700303+Jezreel-Moraes@users.noreply.github.com>
2024-06-30 14:59:35 -0300

    componentizando templates

--- src/javascript/globals.js
+++ src/javascript/globals.js
@@ -1,5 +1,33 @@
+
 async function getData(filename) {
     return fetch(`/src/data/${filename}.json`)
         .then((response) => response.json())
         .then((json) => json);
 }
+
+function LoadTemplatesController() {
+    const parser = new DOMParser();
+
+    async function loadTemplate(path) {
+        return fetch(`/src/templates/${path}`)
+            .then((response) => response.text())
+            .then((html) => parser.parseFromString(html, "text/html").body.firstChild);
+    }
+
+    async function init() {
+        let templateList = document.querySelectorAll(".js-template");
+
+        for (const templatePlaceholder of templateList) {
+            const templateSrc = templatePlaceholder.dataset["src"];
+            const placeholderParent = templatePlaceholder.parentNode;
+            const templateHtml = await loadTemplate(templateSrc)
+
+            placeholderParent.insertBefore(templateHtml, templatePlaceholder);
+            placeholderParent.removeChild(templatePlaceholder);
+        }
+    };
+
+    init();
+}
+
+const loadTemplatesController = new LoadTemplatesController();
